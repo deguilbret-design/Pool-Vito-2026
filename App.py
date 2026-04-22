128364baf6e8af8f025675b9928101e9fbf93e9e
deguilbret-design <deguilbret@gmail.com>
2026-04-21 23:44:36 -0400

    Update App.py

--- App.py
+++ App.py
@@ -20,7 +20,7 @@ st.markdown("""
     .ticker { 
         display: inline-flex; /* Fix pour le défilement mobile */
         white-space: nowrap;
-        animation: ticker 15s linear infinite; 
+        animation: ticker 9s linear infinite; 
     }
     @keyframes ticker { 
         0% { transform: translate3d(0, 0, 0); } 
